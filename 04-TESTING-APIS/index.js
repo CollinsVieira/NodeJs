5fd34384e52a1b0173a8e50d5d2b62ada0eaaab9
CollinsVieira <collins000000000@gmail.com>
2025-12-02 18:54:09 -0500

    testing

--- 04-TESTING-APIS/index.js
+++ 04-TESTING-APIS/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const apiRouter = require('./server')
+const cors = require('cors')
 const { errorLogs, errorHandler } = require('./middleware/error.handler')
 
 const app = express();
 const port = 3000;
 
 app.use(express.json())
+app.use(cors())
 
 app.listen(port, () => {
 })
